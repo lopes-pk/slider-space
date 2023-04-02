fe4d9995794334064c05585d1dd19acecdf7f4a4
lopes-pk <arthurlopes558@gmail.com>
2023-04-02 13:56:31 -0300

    atualizado

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
+const inner = document.querySelector(".inner");
+
 let count = 1;
+
 document.getElementById("radio1").checked = true;
-
-if(count === 1){
-  
-}
-setInterval(function(){
-  nextImage()
-},3000);
-
-function nextImage(){
-  count++;
-  if(count>3){
-    count = 1
+setInterval(function () {
+  inners();
+}, 0);
+function inners() {
+  const checked = document.getElementById("radio1").checked;
+  if (checked) {
+    inner.textContent = "ngc 18";
   }
-  document.getElementById("radio"+count).checked = true;
+  const checked2 = document.getElementById("radio2").checked;
+  if (checked2) {
+    inner.textContent = "nuvem molecular";
+  }
+  const checked3 = document.getElementById("radio3").checked;
+  if (checked3) {
+    inner.textContent = "penhascos cósmicos";
+  }
+}
+setInterval(function () {
+  nextImage();
+}, 7000);
+
+function nextImage() {
+  count++;
+  if (count > 3) {
+    count = 1;
+  }
+  document.getElementById("radio" + count).checked = true;
 }
